ab8aeea67d895749ad13095b7cbcf7bdb7f75d09
Mateusz Skolozynski <mateusz.skolozynski15@gmail.com>
2024-03-17 16:06:21 +0100

    Add GetMax method

--- Lab002/Lab002/Program.cs
+++ Lab002/Lab002/Program.cs
@@ -24,3 +24,23 @@ static double GetAverage(int[] numbers)
 int[] numbers = {1, 2, 3, 4};
 double average = GetAverage(numbers);
 Console.WriteLine(average);
+
+static int GetMax(int[] numbers)
+{
+    int max = numbers[0];
+    foreach (int n in numbers)
+    {
+        if (n > max)
+            max = n;
+    }
+    return max;
+}
+
+// TODO:
+/*
+1. Stwórz nowy branch feature-max
+2. Następnie zaimplementuj opisaną funkcjonalność dodająccommit'y na branch
+3. Na koniec wykonaj merge swojej gałęzi do gałęzi main. Tymrazem spróbuj wykonać merge wykorzystując rebase.
+4. Z pomocą komendy git log sprawdź jak wygląda historiarepozytorium
+5. Wszystkie zmiany powinny zostać wypchnięte do repozytoriumonline
+*/
