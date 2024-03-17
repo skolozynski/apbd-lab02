2254bbf299cc15e7954eaff854968d3d6b294c04
Mateusz Skolozynski <mateusz.skolozynski15@gmail.com>
2024-03-17 16:23:50 +0100

    Change int name

--- Lab002/Lab002/Program.cs
+++ Lab002/Lab002/Program.cs
@@ -27,13 +27,13 @@ Console.WriteLine(average);
 
 static int GetMax(int[] numbers)
 {
-    int max = numbers[0];
+    int maximum = numbers[0];
     foreach (int n in numbers)
     {
-        if (n > max)
-            max = n;
+        if (n > maximum)
+            maximum = n;
     }
-    return max;
+    return maximum;
 }
 
 // TODO:
